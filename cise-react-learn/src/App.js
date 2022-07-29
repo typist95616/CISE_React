cc3dc0742b89981604be725675577b2293ac55bd
Walter Siu <typist95>
2022-07-29 13:26:55 +1200

    Changed word in App.js

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Word Changed~
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Agile is about Values and Principles
         </a>
       </header>
     </div>
